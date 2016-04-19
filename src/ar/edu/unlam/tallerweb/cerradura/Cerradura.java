448a29234a49c03789ff25be382e5f462637fbe7
Gisela <millergiselam@gmail.com>
2016-04-19 15:09:03 -0300

    Cerradura

--- src/ar/edu/unlam/tallerweb/cerradura/Cerradura.java
+++ src/ar/edu/unlam/tallerweb/cerradura/Cerradura.java
@@ -2,29 +2,69 @@ package ar.edu.unlam.tallerweb.cerradura;
 
 public class Cerradura {
 	private Integer claveApertura;
-	private Integer cantidadFallosBloqueo;
-	Integer contador=0;
+	private Integer fallosBloqueo;
+	private Integer contador=0;
+	private Boolean estado=false;
+	private Integer exitosas=0;
+	private Integer fallidas=0;
 	
 	public Cerradura (Integer clave, Integer fallos){
 		claveApertura= clave;
-		cantidadFallosBloqueo= fallos;
+		fallosBloqueo= fallos;
 	}
 	
 	public Boolean abrir(Integer clave){
 		Integer contador=0;
 		if (clave.equals(claveApertura)){
-			return true;
+			contador=0;
+			estado=true;
+			exitosas=exitosas+1;
+			return estado;
 	}
-		contador = contador+1;
-	return false;
-	
+		else{
+			contador=contador+1;
+			estado=false;
+			fallidas=fallidas+1;
+			return estado;			
+		}
 }
-
-	public Boolean fueBloqueada() {
-		if (fallos>=contador){
+	
+	public Boolean cerrar(){
+		if (estado){
+			estado=false;
 			return true;
 		}
 		return false;
-		 
+	}
+	
+	public Boolean estaAbierta(){
+		if (estado){
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean estaCerrada(){
+		if (!estado){
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean fueBloqueada() {
+		if (fallosBloqueo>=contador){
+			return true;
+		}
+		else{
+			return false;
+		} 
+	}
+	
+	public Integer aperturasExitosas(){
+		return exitosas;
+	}
+	
+	public Integer aperturasFallidas(){
+		return fallidas;
 	}
 }
--- test/ar/edu/unlam/tallerweb/cerradura/TestCerradura.java
+++ test/ar/edu/unlam/tallerweb/cerradura/TestCerradura.java
@@ -16,7 +16,7 @@ public class TestCerradura {
 		Assert.assertFalse(miCerradura.abrir(1234));
 	}
 	
-	@Test
+	/*@Test
 	public void testPasarCantidadFallosBloqueo(){
 		Cerradura miCerradura = new Cerradura(1234,3);
 		miCerradura.abrir(7894);
@@ -30,7 +30,52 @@ public class TestCerradura {
 		Cerradura miCerradura = new Cerradura(1234,3);
 		miCerradura.abrir(7894);
 		miCerradura.abrir(7895);
-		Assert.assertFalse(miCerradura.fueBloqueada());
+		miCerradura.abrir(1234);
+		Assert.assertTrue(miCerradura.fueBloqueada());
+	}*/
+	@Test
+	public void testCerrar(){
+		Cerradura miCerradura = new Cerradura(1234,3);
+		miCerradura.abrir(1234);
+		Assert.assertTrue(miCerradura.cerrar());
 	}
-
+	
+	@Test
+	public void testCerraduraEstaAbierta(){
+		Cerradura miCerradura = new Cerradura(1234,3);
+		miCerradura.abrir(1234);
+		Assert.assertTrue(miCerradura.estaAbierta());
+	}
+	@Test
+	public void testCerraduraEstaCerrada(){
+		Cerradura miCerradura = new Cerradura(1234,3);
+		miCerradura.abrir(7894);
+		Assert.assertTrue(miCerradura.estaCerrada());
+	}
+	@Test 
+	public void testCerraduraFueBloqueada(){
+		Cerradura miCerradura = new Cerradura(1234,3);
+		miCerradura.abrir(7894);
+		miCerradura.abrir(7895);
+		miCerradura.abrir(1234);
+		Assert.assertTrue(miCerradura.fueBloqueada());
+	}
+	@Test
+	public void testAperturasExitosas(){
+		Cerradura miCerradura = new Cerradura(1234,3);
+		miCerradura.abrir(1234);
+		miCerradura.abrir(1234);
+		miCerradura.abrir(1234);
+		Integer valorEsperado=3;
+		Assert.assertEquals(valorEsperado, miCerradura.aperturasExitosas());
+	}
+	@Test
+	public void testAperturasFallida(){
+		Cerradura miCerradura = new Cerradura(1234,3);
+		miCerradura.abrir(7894);
+		miCerradura.abrir(7895);
+		miCerradura.abrir(1234);
+		Integer valorEsperado=2;
+		Assert.assertEquals(valorEsperado, miCerradura.aperturasFallidas());
+	}
 }
